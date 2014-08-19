311576825f8fbabce9bfc091e0058c29cda397c3
liu jingyi <songlart@gmail.com>
2014-08-19 20:36:25 +0800

    io.pl     fix tab indent

--- snippets/io.pl
+++ snippets/io.pl
@@ -12,7 +12,7 @@ open(DB,"data") || die qq/Failed to open:$!\n/;
 open(SAVED,">&STDOUT") || die $!,"\n";
 open(STDOUT,">$tmpfile") || die;
 while(<DB>){
-		print SORT;
+	print SORT;
 }
 close SORT;
 open(STDOUT,">&SAVED") || die;
@@ -21,15 +21,15 @@ rename("temp","data");
 
 # the lines in the range from PATTERN until eof(end of file)
 while(<DB>){
-		print if(/PATTERN/ .. eof); 
+	print if(/PATTERN/ .. eof); 
 }
 
 # mark when each file hit eof
 # perl demo.pl file1 file2
 while(<>){
-		print "$.\t$_";
-		if(eof){
-				print '-'x30,"\n";
-				close(ARGV);	# close FH in order to reset the $. back to 1 for the next file that is opened.
-		}
+	print "$.\t$_";
+	if(eof){
+			print '-'x30,"\n";
+			close(ARGV);	# close FH in order to reset the $. back to 1 for the next file that is opened.
+	}
 }
